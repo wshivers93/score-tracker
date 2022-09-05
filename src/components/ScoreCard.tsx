c8b785c4e6152d70523e005fb0a268abd3c65b8c
Will <tayloe93@gmail.com>
2022-09-05 02:12:28 -0500

    Add skeleton of score card

---
+++ src/components/ScoreCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function ScoreCard() {
+  return (
+    <div className="flex flex-col border border-white rounded p-3">
+      <div className="flex flex-row justify-center align-middle">
+        Game Clock
+      </div>
+      <div className="flex flex-row justify-center align-middle">
+        <div className="mx-2 text-center">
+          <div>Team1 Logo</div>
+          <div>Team1 Name</div>
+        </div>
+        <div className="mx-2 text-center">Team1 Score</div>
+        <div className="mx-2 text-center">-</div>
+        <div className="mx-2 text-center">Team2 Score</div>
+        <div className="mx-2 text-center">
+          <div>Team2 Logo</div>
+          <div>Team2 Name</div>
+        </div>
+      </div>
+    </div>
+  );
+}
